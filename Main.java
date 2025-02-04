1f12c7eb3701dce08d2f39ee2575fd1b425be14e
aadgithub1 <152732836+aadgithub1@users.noreply.github.com>
2025-02-04 11:36:44 -0500

    Add getRefString().

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
+    public static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        System.out.println("hello world");
+        int[] refString = getRefString();
+
+        for(int item : refString){
+            System.out.println(item);
+        }
+    }
+
+    public static int[] getRefString(){
+        System.out.println("Enter the reference string: ");
+        String[] userRefString = scanner.nextLine().split("");
+        int[] intUserRefString = new int[userRefString.length];
+        try{
+            for(int i = 0; i < intUserRefString.length; i++){
+                intUserRefString[i] = Integer.parseInt(userRefString[i]);
+            }
+        } catch(NumberFormatException nfe){
+            System.err.println("Invalid data type.");
+        }
+        return intUserRefString;
     }
 }
